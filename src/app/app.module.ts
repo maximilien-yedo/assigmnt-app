9307645648bc12f8f481006166f44bba4e38f939
micbuffa <micbuffa@gmail.com>
2022-02-28 19:55:53 +0100

    Jq section 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule }  from '@angular/material/button';
 import { MatDividerModule }  from '@angular/material/divider';
 import { MatIconModule }  from '@angular/material/icon';
+import { MatInputModule }  from '@angular/material/input';
+import { MatFormFieldModule }  from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { FormsModule } from '@angular/forms'
+
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { RenduDirective } from './shared/rendu.directive';
 @NgModule({
@@ -17,7 +24,9 @@ import { RenduDirective } from './shared/rendu.directive';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatDividerModule, MatIconModule
+    MatButtonModule, MatDividerModule, MatIconModule,
+    FormsModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Assignment } from './assignment.model';
 
 @Component({
   selector: 'app-assignments',
@@ -6,31 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignments.component.css']
 })
 export class AssignmentsComponent implements OnInit {
+  // Champs du formulaire
+  nomAssignment = "";
+  dateDeRenduAssignment!:Date;
+
   titre = "Application de gestion des assignments !"
-  assignments = [
+  assignments:Assignment[] = [
     {
       nom:"Devoir angular pour Mr Buffa",
-      dateDeRendu:"2022-03-01",
+      dateDeRendu: new Date("2022-03-01"),
       rendu:false
     },
     {
       nom:"Devoir Oracle pour Mr Mopolo",
-      dateDeRendu:"2022-01-10",
+      dateDeRendu: new Date("2022-01-10"),
       rendu:true
     },
     {
       nom:"Devoir Grails pour Mr Galli",
-      dateDeRendu:"2022-01-20",
+      dateDeRendu: new Date("2022-01-20"),
       rendu:true
     }
   ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
   getColor(index:number) {
     return index%2 ? 'red' : 'green'
+  }
+
+  onSubmit() {
+    console.log(this.nomAssignment);
+    console.log(this.dateDeRenduAssignment);
+
+    let newAssignment = new Assignment();
+    newAssignment.nom = this.nomAssignment;
+    newAssignment.dateDeRendu = this.dateDeRenduAssignment;
+    newAssignment.rendu = false;
+
+    this.assignments.push(newAssignment)
+
   }
 }
